37cca59f97ca697232258e0405afcd0f930b787a
brunolebrao <bruno.lebrao@gmail.com>
2020-09-02 09:38:56 -0300

    Updates

---
+++ precache-manifest.154ca36edc99122fddf809c49aba93e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1754d4287697020e5bf2b34dd374a79d",
+    "url": "/challenges-pets/index.html"
+  },
+  {
+    "revision": "4e9c3f375c3770f1b647",
+    "url": "/challenges-pets/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "4e9c3f375c3770f1b647",
+    "url": "/challenges-pets/static/js/2.1230f0f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/challenges-pets/static/js/2.1230f0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1470d4db96495824c6",
+    "url": "/challenges-pets/static/js/main.90b229a9.chunk.js"
+  },
+  {
+    "revision": "35cb80e828421db5c6f8",
+    "url": "/challenges-pets/static/js/runtime-main.0a9bae8b.js"
+  }
+]);
